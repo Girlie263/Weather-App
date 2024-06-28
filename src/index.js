fde0f3fdd9b8841a8f3323b16521c0ff3134e3c3
Girlie263 <149056886+Girlie263@users.noreply.github.com>
2024-06-28 18:55:35 +0200

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+function refreshData(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature= response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city){
+    let apiKey ="0cb1t3949f347fc75ae8b7o36a1c0c57";
+    let apiUrl =
+     `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+     axios.get(apiUrl).then(refreshData);
+}
+
+
+
 function handleSubmit(event){
     event.preventDefault()
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form")
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSubmit);
+
+searchCity("Florence")
